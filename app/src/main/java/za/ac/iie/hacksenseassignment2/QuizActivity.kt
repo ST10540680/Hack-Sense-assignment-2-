f1710a8ace93d834fa26da43acfc692bd7c8c6c0
ST10540680 <ST10540680@myemeris.edu.za>
2026-04-20 11:01:53 +0200

    added a function on class QuizActivity

--- app/src/main/java/za/ac/iie/hacksenseassignment2/QuizActivity.kt
+++ app/src/main/java/za/ac/iie/hacksenseassignment2/QuizActivity.kt
@@ -1,6 +1,13 @@
 package za.ac.iie.hacksenseassignment2
 
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_quiz)
+    }
 }
