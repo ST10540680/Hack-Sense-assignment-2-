ff5f9abb9c6d82ae52588bc76bedcf9d1975e136
ST10540680 <ST10540680@myemeris.edu.za>
2026-04-24 21:09:30 +0200

    added a comment and instruction to add score if correct

--- app/src/main/java/za/ac/iie/hacksenseassignment2/QuizActivity.kt
+++ app/src/main/java/za/ac/iie/hacksenseassignment2/QuizActivity.kt
@@ -91,6 +91,10 @@ class QuizActivity: AppCompatActivity() {
         val question = questions [currentIndex]
 
         // compare user answer with correct answer
-        if (userAnswer == question.isTrue)
+        if (userAnswer == question.isTrue) {
+
+            // If correct "increase score"
+            score++
+        }
     }
 }
