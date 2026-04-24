e4d0e7cd72dfc1bf4178ec4be82f57ffd29091fa
ST10540680 <ST10540680@myemeris.edu.za>
2026-04-24 22:07:12 +0200

    added variables

--- app/src/main/java/za/ac/iie/hacksenseassignment2/ScoreActivity.kt
+++ app/src/main/java/za/ac/iie/hacksenseassignment2/ScoreActivity.kt
@@ -1,6 +1,7 @@
 package za.ac.iie.hacksenseassignment2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +11,7 @@ class ScoreActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_score)
 
-        
+        val scoreText = findViewById<TextView>(R.id.scoreText)
+        val feedbackText = findViewById<TextView>(R.id.finalFeedback)
+        val reviewText = findViewById<TextView>(R.id.reviewText)
 }
