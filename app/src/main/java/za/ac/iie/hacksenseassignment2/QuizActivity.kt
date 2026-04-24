c393d7e3bb1709813c8490e9efaaafeb94fcb6e1
ST10540680 <ST10540680@myemeris.edu.za>
2026-04-24 14:42:57 +0200

    added variables and added a new text  view

--- app/src/main/java/za/ac/iie/hacksenseassignment2/QuizActivity.kt
+++ app/src/main/java/za/ac/iie/hacksenseassignment2/QuizActivity.kt
@@ -1,13 +1,15 @@
 package za.ac.iie.hacksenseassignment2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity: AppCompatActivity() {
 
     // Components of the User Interface
-    
+    var questionText = TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
